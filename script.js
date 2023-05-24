f2c39b6442db909c6c7a6cde3cc4da4520f0170e
[Elijah <[efrancis64@gmail.com]>
2023-05-24 14:41:33 -0400

    added questions and choices, added for loop that iterates over questions when start button is pressed

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-var time = document.getElementById('time');
-var startBtn = document.getElementById('start');
-var startScrn = document.getElementById('start-screen')
-var questions = document.getElementById('questions');
+var timeEl = document.getElementById('time');
+var startBtnEl = document.getElementById('start');
+var submitBtnEl = document.getElementById('submit');
+var startScrnEl = document.getElementById('start-screen')
+var questionsEl = document.getElementById('questions');
 
-function startQuiz(event) {
-    event.preventDefault();
-    // startScrn.setAttribute('class', 'hide');
+var questionsArr = [
+    {
+        title: "This is a Test",
+        choices: ['This', 'is', 'a', 'test'],
+        answer: 'this',
+    },
+    {
+        title: "This is 2 Test",
+        choices: ['This', 'is', '2', 'test'],
+        answer: '2',
+    },
+    {
+        title: "This is 3 Test",
+        choices: ['This', 'is', '3', 'test'],
+        answer: '3',
+    },
 
-    // questions.removeAttribute('class', 'hide');
-    var timeLeft = 10;
-    var timer = setInterval(clock, 1000);
-    function clock() {
-        if (timeLeft <= 0){
-            clearInterval(timer);
-            time.innerHTML = "0";
-        } else {
-            time.innerHTML = timeLeft + "";
-        }
-        timeLeft -= 1;
+];
+
+function startQuiz() {
+    startScrnEl.setAttribute('class', 'hide');
+
+    questionsEl.removeAttribute('class', 'hide');
+
+    getQuestions();
+
+    startTime();
+}
+
+function getQuestions() {
+    for (i=0; i<questionsArr.length; i++) {
+        document.getElementById('question').innerHTML = questionsArr[i].title;
+        document.getElementById('choice-one-text').innerHTML = questionsArr[i].choices[0];
+        document.getElementById('choice-two-text').innerHTML = questionsArr[i].choices[1];
+        document.getElementById('choice-three-text').innerHTML = questionsArr[i].choices[2];
+        document.getElementById('choice-four-text').innerHTML = questionsArr[i].choices[3];
     }
 }
 
-startBtn.addEventListener('click', startQuiz);
+function startTime() {
+    var timeLeft = 50;
+    timeEl.innerHTML = 50;
+    var timer = setInterval(countdown, 1000);
+    function countdown() {
+        timeLeft -= 1;
+        if (timeLeft > 0) {
+            timeEl.innerHTML = timeLeft;
+        } else {
+            timeEl.innerHTML = 0;
+            clearInterval(timer);
+        };
+    };
+}
+
+startBtnEl.addEventListener('click', startQuiz);
